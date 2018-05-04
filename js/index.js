ee452021be0b61f5c71d07b0cb3df633624b09a5
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-04 13:36:21 +0200

    zadanie8500-kalkulator działa

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+function download(a) {
+    document.getElementById("display").value+=a;
+    
+}
+function clean() {
+    document.getElementById("display").value="";
+}
+
+function calculate() {
+    var screen = document.getElementById("display").value;
+    document.getElementById("display").value=eval(screen);
+    
+}
